eacb3bf796bdcb6ef1a06abb43c21f61b8c1a4ab
Sebastian Dechant <sebastian.dechant@evan.team>
2019-01-08 16:18:25 +0100

    fix prototype for websocket reconnect

--- initializers/eth.js
+++ initializers/eth.js
@@ -1,12 +1,12 @@
 /*
   Copyright (c) 2018-present evan GmbH.
- 
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
- 
+
       http://www.apache.org/licenses/LICENSE-2.0
- 
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,7 +100,7 @@ module.exports = class Eth extends Initializer {
     }
 
     // connect to web3
-    Web3.providers.WebsocketProvider.prototype.send = (payload, callback) => {
+    Web3.providers.WebsocketProvider.prototype.send = function(payload, callback) {
       let _this = this;
 
       // if the connection is already connecting, wait 100ms and try again
