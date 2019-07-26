77865d114858586684a7792e6659ec9854be57b6
S3bb1 <sebastian.dechant@evan.team>
2019-07-26 13:29:13 +0200

    add config option for gasPrice

--- initializers/smart-agent-core.js
+++ initializers/smart-agent-core.js
@@ -70,14 +70,20 @@ class SmartAgent {
         privateKey: '0x' + api.config.ethAccounts[this.config.ethAccount]
       })
 
+      const runtimeConfig = {
+        accountMap: { [this.config.ethAccount]: api.config.ethAccounts[this.config.ethAccount] },
+        nameResolver: nameResolverConfig,
+        keyConfig: api.config.encryptionKeys
+      }
+
+      if (this.config.runtimeConfig && this.config.runtimeConfig.hasOwnProperty('gasPrice')) {
+        runtimeConfig.gasPrice = this.config.runtimeConfig.gasPrice
+      }
+
       this.runtime = await createDefaultRuntime(
         web3 || api.eth.web3,
         this.dfs,
-        {
-          accountMap: { [this.config.ethAccount]: api.config.ethAccounts[this.config.ethAccount] },
-          nameResolver: nameResolverConfig,
-          keyConfig: api.config.encryptionKeys
-        },
+        runtimeConfig,
         {
           logger: new Logger({ log: api.log, logLevel: 'debug' })
         }
